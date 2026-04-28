d08efd2db62776d351d3c47b5478fbf9860f7ec7
Luis Rodriguez <luedropa@gmail.com>
2026-04-28 15:49:55 -0500

    Corrigiendo detallles de impresion

--- src/claseHashMap.java
+++ src/claseHashMap.java
@@ -3,9 +3,6 @@ import java.util.Map;
 public class claseHashMap {
     public static Map<String, Double> directorioTrabajadores = new HashMap<>();
 
-    public static void actualizarSalario(String nombre){
-        directorioTrabajadores.replace(nombre, 30000.0);
-    }
     public static void main(String[] args) {
 
         directorioTrabajadores.put("Pedro Paramo", 25000.0);
@@ -13,12 +10,17 @@ public class claseHashMap {
         directorioTrabajadores.put("Luis Rodriguez", 40000.0);
         System.out.println("*** Lista de Trabajadores ***");
         directorioTrabajadores.forEach((nombre, salario)->{
-            System.out.printf("Nombre: "+nombre+" Salario: "+salario);
+            System.out.println("Nombre: "+nombre+" Salario: "+salario);
         });
-        claseHashMap.actualizarSalario("Pedro Paramo");
-        System.out.println(directorioTrabajadores.get("Pedro Paramo"));
+        actualizarSalario("Pedro Paramo");
+        System.out.println("===============================");
+        System.out.println("Actualizando salario a pedro paramo");
+        System.out.println("Pedro Paramo: "+directorioTrabajadores.get("Pedro Paramo"));
         System.out.println("**************************");
         System.out.printf("El salrio promedio es %.2f",salarioPromedio(directorioTrabajadores));
+    }
+    public static void actualizarSalario(String nombre){
+        directorioTrabajadores.replace(nombre, 30000.0);
     }
     public static double salarioPromedio(Map directorio){
         double suma = 0;
